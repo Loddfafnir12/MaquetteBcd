e762c8826f0ebd760cd3edef338287a53684d6ae
Vincent <vincent.vanderhoeven10@gmail.com>
2024-11-16 19:19:36 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return <form className="Fond">
+    <div className="Banner">
+      <label htmlFor="Banniere">Banniere</label>
+      <input type="text" id="Banniere" />
+    </div>
+  </form>
+}
