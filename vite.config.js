d146b1601469a5d842ad350453bc13070ed70a84
Vincent <vincent.vanderhoeven10@gmail.com>
2024-11-17 20:41:38 +0100

    message

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    assetsDir: 'assets', // Définir le dossier où seront placés les assets dans dist/
+  },
 })
